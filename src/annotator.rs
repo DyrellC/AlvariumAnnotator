12eaaa1b7f34eb45241e425f2d8ec8e023b1b2cd
DyrellC <dyrell@immutablefutures.com>
2023-11-23 10:53:09 -0700

    Annotator should be mut

--- src/annotator.rs
+++ src/annotator.rs
@@ -1,5 +1,5 @@
 use crate::Annotation;
 
 pub trait Annotator {
-    fn annotate(&self, data: &[u8]) -> Result<Annotation, String>;
+    fn annotate(&mut self, data: &[u8]) -> Result<Annotation, String>;
 }
