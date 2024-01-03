23c36aadf051cfa1b6d76f3a56dd7842bdc17eed
DyrellC <dyrell@immutablefutures.com>
2024-01-02 17:10:17 -0700

    Return changed to Box<dyn stderror>

--- src/annotator.rs
+++ src/annotator.rs
@@ -1,5 +1,5 @@
 use crate::Annotation;
 
 pub trait Annotator {
-    fn annotate(&mut self, data: &[u8]) -> crate::errors::Result<Annotation>;
+    fn annotate(&mut self, data: &[u8]) -> Result<Annotation, Box<dyn std::error::Error>>;
 }
